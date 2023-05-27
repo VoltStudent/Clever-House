e22782869455e47788c5bdc1db9e8aa7dd23d08b
VoltStudent <volt251988@gmail.com>
2023-05-27 19:24:39 +0300

    final

--- js/safe-home.js
+++ js/safe-home.js
@@ -3,17 +3,29 @@ const voltageInput = document.querySelector('.votage');
 const shortCircuitCheckbox = document.querySelector('.short-circuit');
 const signalizationIndicator = document.querySelector('.signalization-indicator');
 const signalizationInput = document.querySelector('.move');
-
+const airAlarm = document.querySelector('.air-alarm');
+const alarmSound = [new Audio('../sound/alarm.mp3'), new Audio('../sound/signalization.mp3'), new Audio('../sound/fire.mp3')]
 
 function changeIndicatorColor(animation) {
     signalizationIndicator.classList.add(animation)
 }
 
+function playAlarmSound(sound) {
+  sound.play();
+}
+
+function stopAlarmSound(sound) {
+  sound.pause();
+  sound.currentTime = 0;
+}
+
 fireCheckbox.addEventListener('change', function () {
     if (fireCheckbox.checked) {
         changeIndicatorColor('fire-color');
+        playAlarmSound(alarmSound[2])
     } else {
         signalizationIndicator.classList.remove('fire-color');
+        stopAlarmSound(alarmSound[2]);
     }
 });
 
@@ -21,6 +33,7 @@ voltageInput.addEventListener('input', function() {
     const voltage = parseInt(voltageInput.value);
     if (voltage < 180 || voltage > 250) {
       changeIndicatorColor('votage-color');
+
     } else {
         signalizationIndicator.classList.remove('votage-color');
     }
@@ -36,8 +49,21 @@ voltageInput.addEventListener('input', function() {
 
   signalizationInput.addEventListener('mouseover', function() {
     changeIndicatorColor('move-color');
+    playAlarmSound(alarmSound[1]);
   });
 
   signalizationInput.addEventListener('mouseout', function() {
     signalizationIndicator.classList.remove('move-color');
+    stopAlarmSound(alarmSound[1]);
   });
+
+  airAlarm.addEventListener('change', function () {
+    
+    if (airAlarm.checked) {
+        changeIndicatorColor('alarm-color');
+        playAlarmSound(alarmSound[0])
+    } else {
+        signalizationIndicator.classList.remove('alarm-color');
+        stopAlarmSound(alarmSound[0]);
+    }
+});
